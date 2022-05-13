e0e3ef4203507b08b375c071baafc72722f5fdf3
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-13 13:01:36 +0600

    testimonial add

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from './Banner';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Service from './Service';
+import Testimonial from './Testimonial';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Info></Info>
             <Service></Service>
             <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
