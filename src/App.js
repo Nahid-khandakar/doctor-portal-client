a87c97d44d2592fe50c9a00e4c2e7aad438af7ff
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-15 19:01:12 +0600

    google sign in add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './pages/About/About';
 import Appointment from './pages/Appointment/Appointment';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 import Navbar from './pages/Shared/Navbar/Navbar';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
 
     </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,16 +6,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
+    apiKey: "AIzaSyALAGeIGxKjQyoLWBBbTXYOTXQioZtexiM",
+    authDomain: "doctor-portal-e2a05.firebaseapp.com",
+    projectId: "doctor-portal-e2a05",
+    storageBucket: "doctor-portal-e2a05.appspot.com",
+    messagingSenderId: "44347709320",
+    appId: "1:44347709320:web:fa7e6f8caa55017d824c3d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
 export default auth;
